a2b0f94d702a2637897957bb0829080bbc65e3e6
Alejandro Villanueva <ing.alejandrovillanueva@gmail.com>
2016-11-22 01:50:10 -0600

    Added font-awesome config to webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -47,10 +47,21 @@ module.exports = function makeWebpackConfig () {
 		  	loader: ExtractTextPlugin.extract('style-loader', 'css-loader!postcss-loader!sass-loader'),
 				test: /\.scss$/,
 			}, {
-				exclude: [/node_modules/],
-	      loader: 'file',
-				test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)$/,
-			}, {
+      	test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+      	loader: "url?limit=10000&mimetype=application/font-woff"
+		  }, {
+		    test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+		    loader: "url?limit=10000&mimetype=application/font-woff"
+		  }, {
+		    test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+		    loader: "url?limit=10000&mimetype=application/octet-stream"
+		  }, {
+		    test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+		    loader: "file"
+		  }, {
+		    test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+		    loader: "url?limit=10000&mimetype=image/svg+xml"
+		  }, {
 				exclude: [/node_modules/],
 				loader: 'raw',
 				test: /\.html$/,
