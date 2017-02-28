ccad5f7b02b331e65e705ac11200fe12be85e67d
Alejandro Villanueva <alejandrovillanueva@17avillanueva.local>
2017-02-28 10:41:09 -0600

    Fixed Carousel component.

--- app/scripts/components/Carousel.ts
+++ app/scripts/components/Carousel.ts
@@ -1,74 +1,97 @@
 import { Swiper } from './Swiper';
 
 class Carousel extends Swiper {
-  static readonly AUTOPLAY_TIME = 1500;
-  static readonly CLONED_ITEM_CLASS = 'item-clone';
+  static readonly AUTOPLAY_TIME = 1200;
+  static readonly CLONED_CLASS = 'clone';
 
-  private interval;
+  private interval: number;
+  private isPartialItem: boolean;
 
   constructor(element) {
     super(element);
     this.createClones = this.createClones.bind(this);
+    this.showNext = this.showNext.bind(this);
+    this.stopAutoplay = this.stopAutoplay.bind(this);
+    this.autoplay = this.autoplay.bind(this);
+    this.isPartialItem = false;
     this.createClones();
-    this.interval = this.autoplay();
-    this.container.addEventListener(this.supportEvents.move, () => {
-      clearInterval(this.interval);
-    });
-    this.container.addEventListener('mouseout', () => {
-      this.interval = this.autoplay();
-    });
-
+    this.autoplay();
+    this.container.addEventListener(this.supportEvents.move, this.stopAutoplay);
+    this.container.addEventListener('mouseout', this.autoplay);
     window.onResize(this.createClones, 1);
   }
 
-  public autoplay() {
-    return setInterval(() => {
+  public autoplay(): void {
+    this.interval = setInterval(() => {
       this.showNext();
       this.prevCtrl.classList.add(Swiper.ACTIVE_CTRL_CLASS);
       this.nextCtrl.classList.add(Swiper.ACTIVE_CTRL_CLASS);
     }, Carousel.AUTOPLAY_TIME);
   }
 
-  public showNext() {
-    this.index = ++this.index;
+  public stopAutoplay(): void {
+    clearInterval(this.interval);
+  }
 
-    if (this.index <= this.lastToShow() + 1) {
+  public showNext(): void {
+    this.index = ++this.index;
+    let lastToShow = this.lastToShow() + 1;
+
+    if (this.index <= lastToShow) {
       let currentItem = this.items[this.index] as HTMLElement;
 
       this.animate(currentItem.offsetLeft, Swiper.ANIMATION_MS);
 
-      if (this.index === this.lastToShow() + 1) {
-        setTimeout(() => {
-          this.animate(0, 0);
-          this.index = 0;
-        }, Swiper.ANIMATION_MS);
+      if ((this.isPartialItem && this.index === lastToShow - 1)
+        || this.index === lastToShow) {
+        this.restartPosition();
       }
     }
   }
 
   public createClones(): void {
-    let clonedItems = this.container
-      .querySelectorAll(`.${ Carousel.CLONED_ITEM_CLASS }`);
-    let itemsLength = this.items.length - 1;
-    let numToCloned = itemsLength - this.lastToShow();
+    let clons = this.container.querySelectorAll(`.${ Carousel.CLONED_CLASS }`);
+    let last = this.items[this.items.length - 1] as HTMLElement;
+    let clonedAmount = this.items.length - 1 - this.lastToShow();
 
-    for (let i = 0; i < numToCloned; i++) {
+    if (last.offsetWidth < this.container.offsetWidth && clonedAmount === 1) {
+      this.isPartialItem = true;
+      clonedAmount = ++clonedAmount;
+    } else {
+      this.isPartialItem = false;
+    }
+
+    for (let i = 0; i < clonedAmount; i++) {
       let currentItem = this.items[i] as HTMLElement;
 
-      if (currentItem.offsetLeft <= this.container.offsetWidth) {
-        let newNode = currentItem.cloneNode(true);
-        let newItem = newNode as HTMLElement;
-
-        newItem.classList.add(Carousel.CLONED_ITEM_CLASS);
-        this.container.appendChild(newItem);
+      if (currentItem.offsetLeft > this.container.offsetWidth) {
+        break;
       }
+
+      let clonedItem = currentItem.cloneNode(true) as HTMLElement;
+
+      clonedItem.classList.add(Carousel.CLONED_CLASS);
+      this.container.appendChild(clonedItem);
     }
 
-    for (let i = 0; i < clonedItems.length; i++) {
+    this.removeCloned(clons);
+    this.items = this.container.querySelectorAll(`.${ Swiper.ITEM_CLASS }`);
+  }
+
+  public removeCloned(clonedItems) {
+    let clonedAmount = clonedItems.length;
+
+    for (let i = 0; i < clonedAmount; i++) {
       clonedItems[i].parentNode.removeChild(clonedItems[i]);
     }
+  }
 
-    this.items = this.container.querySelectorAll(`.${ Swiper.ITEM_CLASS }`);
+  public restartPosition(): void {
+    let timeout = setTimeout(() => {
+      this.animate(0, 0);
+      this.index = 0;
+      clearTimeout(timeout);
+    }, Swiper.ANIMATION_MS);
   }
 }
 
