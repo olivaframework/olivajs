8a17fe48da10e0c1907af5c80161ff3db28c9ae0
Alejandro Villanueva <ing.alejandrovillanueva@gmail.com>
2017-05-17 01:24:40 -0500

    Added update swiper by event on tab.

--- app/scripts/Tab.ts
+++ app/scripts/Tab.ts
@@ -1,4 +1,5 @@
 import { DOMUtils } from './DOMUtils';
+import { Swiper } from './Swiper';
 
 class Tab {
   static readonly ACTIVE_CLASS: string = 'active';
@@ -7,6 +8,7 @@ class Tab {
 
   public handler: HTMLAnchorElement;
   public content: Element;
+  public swipers: NodeListOf<Element>;
 
   constructor(handler: HTMLAnchorElement) {
     this.handler = handler;
@@ -15,17 +17,32 @@ class Tab {
     this.handler.addEventListener(Tab.EVENT_ACTIVE, this.toggle);
   }
 
+  public updateSwipers() {
+    const swipers = this.content
+      .querySelectorAll(`[${ Swiper.SWIPER_UID_ATTR }]`);
+
+    DOMUtils.syncForEach(swiper => {
+      const uId = swiper.getAttribute(Swiper.SWIPER_UID_ATTR);
+
+      DOMUtils.dispatchCustomEvent(uId, swiper);
+    }, swipers);
+  }
+
   public toggle(event): void {
     event.preventDefault();
 
-    const contentItems = this.content.parentElement.children;
-    const handlerItems = this.handler.parentElement.children;
+    if (!DOMUtils.containsClass(this.handler, Tab.ACTIVE_CLASS)) {
+      const contentItems = this.content.parentElement.children;
+      const handlerItems = this.handler.parentElement.children;
 
-    DOMUtils.removeClassToItems(contentItems, Tab.ACTIVE_CLASS);
-    DOMUtils.removeClassToItems(handlerItems, Tab.ACTIVE_CLASS);
+      DOMUtils.removeClassToItems(contentItems, Tab.ACTIVE_CLASS);
+      DOMUtils.removeClassToItems(handlerItems, Tab.ACTIVE_CLASS);
 
-    DOMUtils.addClass(this.content, Tab.ACTIVE_CLASS);
-    DOMUtils.addClass(this.handler, Tab.ACTIVE_CLASS);
+      DOMUtils.addClass(this.content, Tab.ACTIVE_CLASS);
+      DOMUtils.addClass(this.handler, Tab.ACTIVE_CLASS);
+
+      this.updateSwipers();
+    }
   }
 }
 
